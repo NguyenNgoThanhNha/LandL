76d524c877eb43df35230db93da85fe1cdd04763
NguyenNha <nguyenngothanhnha2003kg@gmail.com>
2024-09-16 16:54:38 +0700

    Accept order for driver success

--- server/L&L.Business/Services/OrderService.cs
+++ server/L&L.Business/Services/OrderService.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using L_L.Business.Commons.Request;
+using L_L.Business.Exceptions;
 using L_L.Business.Models;
 using L_L.Business.Ultils;
 using L_L.Data.Entities;
@@ -71,13 +73,35 @@ namespace L_L.Business.Services
                 .Include(o => o.DeliveryInfoDetail)  // Include bảng DeliveryInfo
                 /*                .Include(o => o.TruckInfo)           // Include bảng Truck*/
                 .Include(o => o.ProductInfo)         // Include bảng Product
-                /*                .Include(o => o.OrderInfo)           // Include bảng Order*/
+                .Include(o => o.OrderInfo)           // Include bảng Order
                 /*                .Include(o => o.UserOrder)           // Include bảng User*/
                 .Where(o => o.Status == StatusEnums.Processing.ToString())
                 .ToListAsync();
             return _mapper.Map<List<OrderDetailsModel>>(listOrderDetail);
         }
 
+        public async Task<bool> AddDriverToOrderDetail(AcceptDriverRequest req)
+        {
+            // get order detail
+            var orderDetail = await unitOfWorks.OrderDetailRepository.GetByIdAsync(int.Parse(req.orderDetailId));
+            if (orderDetail == null)
+            {
+                throw new BadRequestException("Order detail not found!");
+            }
 
+            var order = await unitOfWorks.OrderRepository.GetByIdAsync(int.Parse(req.orderDetailId));
+            if (order?.OrderId != orderDetail.OrderId)
+            {
+                throw new BadRequestException("Order detail of order are invalid!");
+            }
+
+            order.DriverId = int.Parse(req.driverId);
+
+            var orderUpdate = unitOfWorks.OrderRepository.Update(order);
+
+            var result = await unitOfWorks.OrderRepository.Commit();
+
+            return result > 0;
+        }
     }
 }
